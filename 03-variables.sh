cd5c2d3f7d5b98ad67aa791d830e69ae75799e14
chandanaputtap <chandana.mech.jntp@gmail.com>
2026-01-12 13:54:48 -0500

    Added files

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Chandana
+
+echo "$PERSON1: Hello ${PERSON2}, How are you?"
+echo "${PERSON2}: Hi $PERSON1, I am doing good, Thank you. How are you doing?"
+echo "$PERSON1: What are you doing? How is your work?"
+echo "$PERSON2: Not bad, Planning to upskill. How about you?"
